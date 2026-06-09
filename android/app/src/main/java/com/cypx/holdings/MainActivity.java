6beee9dc10376ee55709252026e66ad9044d960f
CYPX ONLINE CRYPTO <quantumriokenya@gmail.com>
2026-06-09 17:04:06 +0000

    Add exit confirmation dialog

--- android/app/src/main/java/com/cypx/holdings/MainActivity.java
+++ android/app/src/main/java/com/cypx/holdings/MainActivity.java
@@ -1,19 +1,30 @@
 package com.cypx.holdings;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
         public void onBackPressed() {
-                if (bridge != null &&
-                            bridge.getWebView() != null &&
-                                        bridge.getWebView().canGoBack()) {
 
-                                                    bridge.getWebView().goBack();
-                                                            } else {
-                                                                        super.onBackPressed();
-                                                                                }
-                                                                                    }
-                                                                                    }
+                if (bridge != null &&
+                                bridge.getWebView() != null &&
+                                                bridge.getWebView().canGoBack()) {
+
+                                                            bridge.getWebView().goBack();
+
+                                                                    } else {
+
+                                                                                new AlertDialog.Builder(this)
+                                                                                                    .setTitle("Exit App")
+                                                                                                                        .setMessage("Do you want to exit the app?")
+                                                                                                                                            .setCancelable(true)
+                                                                                                                                                                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                                                                                                                                                                                    .setPositiveButton("Exit", (dialog, which) -> finish())
+                                                                                                                                                                                                        .show();
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                    }
