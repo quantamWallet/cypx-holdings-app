18cbff0bf9cb2a89654a80ab44ac93199ece0a24
CYPX ONLINE CRYPTO <quantumriokenya@gmail.com>
2026-06-09 14:38:33 +0000

    Fix Android back button

--- android/app/src/main/java/com/cypx/holdings/MainActivity.java
+++ android/app/src/main/java/com/cypx/holdings/MainActivity.java
@@ -1,5 +1,19 @@
 package com.cypx.holdings;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+        public void onBackPressed() {
+                if (bridge != null &&
+                            bridge.getWebView() != null &&
+                                        bridge.getWebView().canGoBack()) {
+
+                                                    bridge.getWebView().goBack();
+                                                            } else {
+                                                                        super.onBackPressed();
+                                                                                }
+                                                                                    }
+                                                                                    }
